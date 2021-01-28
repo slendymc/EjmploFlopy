5a81f28565e84c63219f60d65247a472438facd0
Slendy Mantilla <SLENDY.MANTILLA@correo.uis.edu.co>
2021-01-28 14:11:39 -0500

    Add datos principales

--- temp.py
+++ temp.py
@@ -1,11 +1,19 @@
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-import flopy# -*- coding: utf-8 -*-
-
 """
 Spyder Editor
 
 This is a temporary script file.
 """
 
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy# -*- coding: utf-8 -*-
+
+name = "Ejercicio_01_1"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
