a0e8e1cd1eeda949c4f0d01a43a18e04449a1056
Slendy Mantilla <SLENDY.MANTILLA@correo.uis.edu.co>
2021-01-28 22:49:19 -0500

    gregue comandos sim, tdsi, ims, gwf

--- temp.py
+++ temp.py
@@ -9,6 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import flopy# -*- coding: utf-8 -*-
 
+
+#parametros iniciales
 name = "Ejercicio_01_1"
 h1 = 100
 h2 = 90
@@ -18,5 +20,21 @@ L = 400.0
 H = 50.0
 k = 1.0
 
+#Busca mf6 en la carpeta especificada y guarda los archivos
+#Crea objetos de flopy TDIS
 sim = flopy.mf6.MFSimulation(
-    sim_name=name, exe_name="mf6", version="mf6", sim_ws=".")
+    sim_name=name, exe_name="C:\WRDAPP\mf6.2.0 (1)\mf6.2.0\bin", version="mf6", sim_ws="Workspace"
+    ) 
+
+#Crea el paquete de objetos de flopy TDIS
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
+)
+
+
+#crea el paquete de objetos de flopy IMS
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+#crea el modelo de flujo de agua
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
