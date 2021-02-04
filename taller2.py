110358a1d45bab02142e92f688b93a0f7224d160
Slendy Mantilla <SLENDY.MANTILLA@correo.uis.edu.co>
2021-02-04 16:07:31 -0500

    cambios nuevos

---
+++ taller2.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 15:42:19 2021
+
+@author: Lenovo
+"""
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import flopy
+
+name = "ejercicio1"
+h1 = 100
+h2 = 90
+Nlay = 10
+N = 101
+L = 400.0
+H = 50.0
+k = 1.0
+
+sim = flopy.mf6.MFSimulation(
+    sim_name=name, exe_name=
+    "C:/Users/Lenovo/Desktop/Diapositivas PyS/DIPLOMADO/mf6.2.0/bin/mf6",
+    version="mf6", sim_ws="workspace"
+) 
+
+#guarda mf6 en la carpeta especfica y guarada los archivos
+
+tdis = flopy.mf6.ModflowTdis(
+    sim, pname="tdis", time_units="DAYS", nper=1, perioddata=[(1.0, 1, 1.0)]
+)
+
+#Crea el TDISobjeto Flopy
+ims = flopy.mf6.ModflowIms(sim, pname="ims", complexity="SIMPLE")
+
+#Crea el IMSobjeto Flopy Package
+
+
+
+model_nam_file = "{}.nam".format(name)
+gwf = flopy.mf6.ModflowGwf(sim, modelname=name, model_nam_file=model_nam_file)
+
+#Crear el objeto de modelo de flujo de agua subterránea Flopy (gwf)
+
+#Crear el DISpaquete de discretización
+
+bot = np.linspace(-H / Nlay, -H, Nlay)
+delrow = delcol = L / (N - 1) #espesro de filas
+dis = flopy.mf6.ModflowGwfdis(  #paquete de discretizacion
+    gwf,
+    nlay=Nlay,
+    nrow=N,
+    ncol=N,
+    delr=delrow,
+    delc=delcol,
+    top=0.0,
+    botm=bot,
+)
+
+#Crear el ICpaquete de condiciones iniciales ( )
+start = h1 * np.ones((Nlay, N, N))
+ic = flopy.mf6.ModflowGwfic(gwf, pname="ic", strt=start)
+
+#Crear el NPFpaquete de flujo de propiedades de nodo
+
+
+k=np.ones([10,N,N])
+k[1,:,:]=5e-1
+
+
+
+npf = flopy.mf6.ModflowGwfnpf(gwf, icelltype=1, k=k, save_flows=True)
+
+
+
+#Crear el CHDpaquete de cabeza constante ( )
+
+chd_rec = []
+chd_rec.append(((0, int(N / 4), int(N / 4)), h2))
+chd_rec.append(((1, int(3*N / 4), int(3*N / 4)), h2-3))
+for layer in range(0, Nlay):
+    for row_col in range(0, N):
+        chd_rec.append(((layer, row_col, 0), h1))
+        chd_rec.append(((layer, row_col, N - 1), h1))
+        if row_col != 0 and row_col != N - 1:
+            chd_rec.append(((layer, 0, row_col), h1))
+            chd_rec.append(((layer, N - 1, row_col), h1))
+chd = flopy.mf6.ModflowGwfchd(
+    gwf,
+    maxbound=len(chd_rec),
+    stress_period_data=chd_rec,
+    save_flows=True,
+)
+
+#l CHDpaquete almacenaba las cabezas constantes en una matriz estructurada, también llamada numpy.recarray. Podemos obtener un puntero al recarray para el primer período de estrés (iper = 0) de la siguiente manera.
+iper = 0
+ra = chd.stress_period_data.get_data(key=iper)
+ra
+
+
+# Create the output control (`OC`) Package
+headfile = "{}.hds".format(name)
+head_filerecord = [headfile]
+budgetfile = "{}.cbb".format(name)
+budget_filerecord = [budgetfile]
+saverecord = [("HEAD", "ALL"), ("BUDGET", "ALL")]
+printrecord = [("HEAD", "LAST")]
+oc = flopy.mf6.ModflowGwfoc(
+    gwf,
+    saverecord=saverecord,
+    head_filerecord=head_filerecord,
+    budget_filerecord=budget_filerecord,
+    printrecord=printrecord,
+)
+
+#Escribe los conjuntos de datos
+
+sim.write_simulation()
+
+#Ejecuta la simulación
+
+success, buff = sim.run_simulation()
+if not success:
+    raise Exception("MODFLOW 6 did not terminate normally.")
+    
+#Trazar un mapa de la capa 1
+headfile=  'Workspace' +'/'+headfile
+hds = flopy.utils.binaryfile.HeadFile(headfile)
+h = hds.get_data(kstpkper=(0, 0))
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[0], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%2.1f")
+
+
+#Trazar un mapa de la capa 10
+
+x = y = np.linspace(0, L, N)
+y = y[::-1]
+fig = plt.figure(figsize=(6, 6))
+ax = fig.add_subplot(1, 1, 1, aspect="equal")
+c = ax.contour(x, y, h[-1], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
+
+
+z = np.linspace(-H / Nlay / 2, -H + H / Nlay / 2, Nlay)
+fig = plt.figure(figsize=(5, 2.5))
+ax = fig.add_subplot(1, 1, 1, aspect="auto")
+c = ax.contour(x, z, h[:, 50, :], np.arange(90, 100.1, 0.2), colors="black")
+plt.clabel(c, fmt="%1.1f")
+
+
